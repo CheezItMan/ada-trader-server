353cff6699a4259db4c071b745b1dd6093331fa3
CheezItMan <chris@adadevelopersacademy.org>
2017-12-08 10:23:53 -0800

    allow blank names

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,4 @@
 class Stock < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
   validates :symbol, presence: true, length: {minimum: 2, maximum: 4}, uniqueness: true
   validates :price, presence: true
   validates :price, numericality: true
